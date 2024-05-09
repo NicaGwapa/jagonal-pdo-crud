237e3c0c5cd6970b17c0dbec4fc53366656c15e4
NicaGwapa <dahinonica@gmail.com>
2024-05-09 10:00:04 +0800

    updated create page

--- public/create.php
+++ public/create.php
@@ -53,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Records created successfully. Redirect to index.php
-                header("location: ./index.php");
+                header("location: ../public/welcome.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
@@ -106,7 +106,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <span class="invalid-feedback"><?php echo $product_retail_price_err;?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="./index.php" class="btn btn-secondary ml-2">Cancel</a>
+                        <a href="../public/welcome.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
                 </div>
             </div>        
